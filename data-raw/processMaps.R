4c18c8640c9087b60b314320648a41d78e041daa
Ernest Guevarra <ernest@guevarra.io>
2021-01-08 11:36:20 +0000

    edit CRS of spatial objects

--- data-raw/processMaps.R
+++ data-raw/processMaps.R
@@ -5,23 +5,31 @@ library(raster)
 
 ## Read maps ###################################################################
 
+## Livelihood zones ------------------------------------------------------------
+livelihood_zone <- readOGR(dsn = "data-raw/TZ_LHZ_2009", layer = "TZ_LHZ_2009")
+usethis::use_data(livelihood_zone, overwrite = TRUE, compress = "xz")
+
 ## Region ----------------------------------------------------------------------
 region <- readOGR(dsn = "data-raw/GIS_Maps", layer = "Regions")
+region <- spTransform(x = region, CRSobj = CRS(proj4string(livelihood_zone)))
 usethis::use_data(region, overwrite = TRUE, compress = "xz")
 
 ## District --------------------------------------------------------------------
 district <- readOGR(dsn = "data-raw/Districts_Shapefiles_2019",
                     layer = "Districts and TC as 2020")
+district <- spTransform(x = district, CRSobj = CRS(proj4string(livelihood_zone)))
 usethis::use_data(district, overwrite = TRUE, compress = "xz")
 
 ## Ward ------------------------------------------------------------------------
 ward <- readOGR(dsn = "data-raw/2012 Wards Shapefiles",
                 layer = "TZwards")
+ward <- spTransform(x = ward, CRSobj = CRS(proj4string(livelihood_zone)))
 usethis::use_data(ward, overwrite = TRUE, compress = "xz")
 
 ## Village ---------------------------------------------------------------------
 village <- readOGR(dsn = "data-raw/2002/Tanzania_Village_EA_2002_region",
                    layer = "Tanzania_Village_EA_2002_region")
+village <- spTransform(x = village, CRSobj = CRS(proj4string(livelihood_zone)))
 usethis::use_data(village, overwrite = TRUE, compress = "xz")
 
 ## Global ruminant population --------------------------------------------------
@@ -29,9 +37,7 @@ x <- raster::raster("data-raw/2_GlobalRuminantLPS_GIS/glps_gleam_61113_10km.tif"
 ruminant_population <- raster::intersect(x, region)
 usethis::use_data(ruminant_population, overwrite = TRUE, compress = "xz")
 
-## Livelihood zones ------------------------------------------------------------
-livelihood_zone <- readOGR(dsn = "data-raw/TZ_LHZ_2009", layer = "TZ_LHZ_2009")
-usethis::use_data(livelihood_zone, overwrite = TRUE, compress = "xz")
+
 
 
 
